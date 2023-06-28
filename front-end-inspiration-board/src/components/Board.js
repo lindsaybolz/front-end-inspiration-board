b4cc70012688f36e5489151ca4102d9bb6096b2b
Winslow <owedwards@gmail.com>
2023-06-28 14:57:02 -0400

    BoardList and Board updated

--- front-end-inspiration-board/src/components/Board.js
+++ front-end-inspiration-board/src/components/Board.js
@@ -5,12 +5,13 @@ import PropTypes from 'prop-types';
 const Board = ({ board_id, owner, title }) => {
     return (
         <div>
-            <select
+            {board_id} {title} {owner}
+            {/* <select
             id={board_id}
-            options={title}
+            options={board_id} {title} {owner}
             // onChange={handleChange}
             >
-            </select>
+            </select> */}
         </div>
     )
 } 
--- front-end-inspiration-board/src/components/BoardList.js
+++ front-end-inspiration-board/src/components/BoardList.js
@@ -1,9 +1,37 @@
 import './BoardList.css';
 import React from 'react';
 import PropTypes from 'prop-types';
+import Board from './Board';
 
-const BoardList = (props) => {
-    return
-} 
+const BoardList = ({ boards }) => {
+    const boardComponents = boards.map(boardInstance => {
+        return(
+            <li key={boardInstance.id}>
+                <Board
+                board_id={boardInstance.board_id}
+                owner={boardInstance.owner}
+                title={boardInstance.title}
+                />
+            </li>
+    )})
+    
+    return (
+        <section>
+            <ul>
+                {boardComponents}
+            </ul>
+        </section>
+    )
+}
+
+BoardList.propTypes = {
+    boards: PropTypes.arrayOf(
+        PropTypes.shape({
+            board_id: PropTypes.number.isRequired,
+            owner: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+        })
+    )
+}
 
 export default BoardList;
