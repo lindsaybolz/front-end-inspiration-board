2f88e77e41369f26045ee243338e707a1f85e9df
Winslow <owedwards@gmail.com>
2023-06-28 11:28:21 -0400

    Started board function

--- front-end-inspiration-board/src/components/Board.js
+++ front-end-inspiration-board/src/components/Board.js
@@ -2,8 +2,16 @@ import './Board.css'
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Board = (props) => {
-    return
+const Board = ({ board_id, owner, title }) => {
+    return (
+        <div>
+            <select
+            id={board_id}
+            options={title}
+            onChange={handleChange}>
+            </select>
+        </div>
+    )
 } 
 
 export default Board;
