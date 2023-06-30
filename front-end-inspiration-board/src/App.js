ee857212ef2840bf494c260f72b57b0133f1c5e0
WinsEdwards <127224520+WinsEdwards@users.noreply.github.com>
2023-06-30 14:40:02 -0700

    Updated App.js to invoice NewBoardForm and to include post functionality with that

--- front-end-inspiration-board/src/App.js
+++ front-end-inspiration-board/src/App.js
@@ -41,13 +41,32 @@ function App() {
 
   const [cards, setCards] = React.useState([])
 
+  const addBoard = (newBoardData) => {
+    axios
+      .post(`http://localhost:5000/boards`, newBoardData)
+      .then((response) => {
+        const newBoards = [...Boards];
+
+        newBoards.push({
+          id: response.data.id,
+          owner: response.data.owner,
+          title: response.data.title,
+        });
+
+        setBoards(newBoards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <main className='App'>
       <h1>Inspiration Board</h1>
       <BoardList
         boards={boards}
       />
-      {/* <NewBoardForm addBoardCallback={addBoard} /> */}
+      <NewBoardForm addNewdBoardCallback={addBoard} />
       <Board />
       {/* <NewCardForm addCardCallback={addCard} /> */}
       <CardList
