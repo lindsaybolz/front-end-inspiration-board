f2688fa7f95eee065038cd0054776ca9d5248e23
Winslow <owedwards@gmail.com>
2023-07-18 10:30:36 -0400

    NewCardForm created and functioning

--- front-end-inspiration-board/src/components/NewCardForm.js
+++ front-end-inspiration-board/src/components/NewCardForm.js
@@ -2,8 +2,55 @@ import './NewCardForm.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NewCardForm = (props) => {
-    return
-} 
+const NewCardForm = ({ addNewCardCallback }) => {
+    const [formFields, setFormFields] = React.useState({
+        message: ''
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        addNewCardCallback(formFields);
+
+        setFormFields({
+            message: ''
+        });
+    };
+
+    const handleChange = (evt) => {
+        setFormFields({...formFields, [evt.target.name]: evt.target.value})
+        
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className='new_card_form'>
+            <section>
+                <h2>Create a new card</h2>
+                <div className='new_card_fields'>
+                    <div>
+                    <label htmlFor='message'>Message: </label>
+                    <input
+                        name='message'
+                        value={formFields.message}
+                        onChange={handleChange}
+                    />
+                    </div>
+                    <button
+                        className='button new_card_submit'
+                        type='submit'
+                        value='Add New Card'
+                    >
+                        Add Card
+                    </button>
+                </div>
+            </section>
+        </form>
+    );
+};
+
+NewCardForm.propTypes = {
+    addNewCardCallback : PropTypes.func.isRequired,
+}; 
+
 
 export default NewCardForm;
